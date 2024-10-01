f15233f3618c8246008613df0ac2566d0f129f4a
Carmen Luna <lunacarmen22@gmail.com>
2024-09-30 20:59:58 -0500

    fix: seach imput

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -8,7 +8,32 @@ export default getRequestConfig(async ({ locale }) => {
     notFound();
   }
 
+  // Cargar varios archivos de traducción
+  const messages = {
+    ...(await import(`../public/messages/${locale}/home.json`)).default,
+    ...(await import(`../public/messages/${locale}/footer.json`)).default,
+    ...(await import(`../public/messages/${locale}/header.json`)).default,
+    ...(await import(`../public/messages/${locale}/destination.json`)).default,
+    ...(await import(`../public/messages/${locale}/others.json`)).default,
+  };
+
   return {
-    messages: (await import(`../public/messages/${locale}.json`)).default,
+    messages
   };
 });
+
+
+// import { getRequestConfig } from "next-intl/server";
+// import { notFound } from "next/navigation";
+
+// const locales = ["en", "es"];
+
+// export default getRequestConfig(async ({ locale }) => {
+//   if (!locales.includes(locale)) {
+//     notFound();
+//   }
+
+//   return {
+//     messages: (await import(`../public/messages/${locale}.json`)).default,
+//   };
+// });
